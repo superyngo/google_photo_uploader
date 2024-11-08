3f53b0a7ed72b45b3c9e0636d66ba305f474711a
superyngo <superyngo@gmail.com>
2024-11-08 22:32:53 +0800

    update

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 from modules.uploader import *
 
+
 CsUCUploader = cs_factory(dic_uploader_config)
 UCuploader = CsUCUploader()
-UCuploader.login()
-UCuploader.upload_to_google_photo(True)
-UCuploader.upload_to_google_photo()
-UCuploader.quit()
+
+menu = {
+    'login': UCuploader.login,
+    'register': UCuploader.register_config,
+    'upload': UCuploader.upload_to_google_photo,
+}
+input = None
+
+while True:
+    task = input(f"tasks:\n {"\n ".join(menu.keys())}\n")
+    if input != 'quit':
+        menu(task)()
+    else:
+        break
 
