835a2d4f1dabb8534ccb18eab3ba87a32482fb74
superyngo <superyngo@gmailc.om>
2024-11-20 13:58:46 +0800

    ffmpeg scrips

--- scripts/ff_somescript.bat
+++ scripts/ff_somescript.bat
@@ -1,5 +1,21 @@
-1:50:24
-3:16:36
+ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]setpts=0.01*PTS[v]                           ;[0:a]atempo=100[a]                            " -map "[v]" -map "[a]" output.mkv
+
+ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]setpts=PTS/(if(gte(mod(N\,2382)\,40)\,100\,1))[v];[0:a]atempo=(if(gte(mod(N\,2382)\,40)\,100\,1))[a]" -map "[v]" -map "[a]" output.mkv
+ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]setpts=PTS/(if(gte(mod(N\,2382)\,40)\,100\,1))[v]" -an -map "[v]" output.mkv
+ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]setpts=PTS*if(gte(mod(N\,2382)\,40)\,0.01\,1)[v]" -an -map "[v]" output.mkv
+
+ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]setpts=if(gte(mod(N\,2382)\,40)\,0.01\,1)*PTS[v]" -map "[v]" -an output.mkv
+
+ffmpeg -i 20241117_cut.mp4 -vf "setpts=0.01*PTS" -af "atempo=100" output.mkv
+
+ffmpeg -i 20241117_cut.mp4 -vf "setpts='if(gte(mod(N,2382),40),0.01*PTS,PTS)'" -an output.mkv
+asetpts='if(lt(N,10),PTS+3/TB,PTS)'
+
+ffmpeg -i 20241117_cut.mp4 -filter_complex "
+[0:v]setpts=if(gte(mod(n,2382),40),0.01*PTS[v],PTS[v]):if(gte(t,10),-h,-h)
+" output.mp4
+
+ffmpeg -i 20241117_cut.mp4 -vf "select='if(lte(mod(n,2382),39.7),1,not(mod(n,99.25)))',setpts=N/FRAME_RATE/TB" -af "aselect='if(lte(mod(n,2382),39.7),1,not(mod(n,99.25)))',asetpts=N/SR/TB" -shortest -vsync vfr output.mkv
 
 ffmpeg -i input.mkv -filter_complex "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2[a]" -map "[v]" -map "[a]" output.mkv
 
@@ -18,4 +34,20 @@ ffmpeg -i 20241117_cut.mkv -vf "select='gte(mod(n,99),77)',setpts=N/FRAME_RATE/T
 ffmpeg -i 20241117_cut.mp4 -filter_complex "[0:v]select='gte(mod(n,99),77)',setpts=0.01*PTS[v];[0:a]aselect='gte(mod(n,99),77)',atempo=100[a]" -map "[v]" -map "[a]" output.mkv
 
 
+
+ffprobe -i 20241117_cut.mp4
+
+ffmpeg -i 20241117_cut.mp4 -filter:v "fps=20" -c:v libx264  -preset fast -crf 23 -c:a copy output.mp4
+
+ffmpeg -i 20241117_cut.mp4 -vf "
+select='if(lte(mod(n,18.95*180),18.95*3),1,not(mod(n,18.95*5)))',setpts=N/FRAME_RATE/TB
+" -af "
+aselect='if(lte(mod(n,44100*180),44100*3),1,not(mod(n,44100*5)))',asetpts=N/SR/TB
+" output.mkv
+
+ffmpeg -i 20241117_cut.mp4 -vf "
+select='if(lte(mod(t,180),3),1,not(mod(n,100)))',setpts=N/FRAME_RATE/TB
+" -af "
+aselect='if(lte(mod(t,180),3),1,not(mod(n,100)))',asetpts=N/SR/TB
+" -shortest output.mkv
 
--- scripts/ff_speedup_audio_atempo.bat
+++ scripts/ff_speedup_audio_atempo.bat
@@ -10,20 +10,22 @@ for /f "tokens=1,2 delims=." %%a in ("%speedup_ratio%") do (
     set "fraction=%%b"
 )
 set "video_pts=1.%fraction%/%integer%"
+set "filter_complex_expr=[0:v]setpts=!video_pts!*PTS[v];[0:a]atempo=!speedup_ratio![a]" 
+set "postfix=speedup"
 
 REM Check if files were dragged and dropped
 if "%~1"=="" (
     echo No files dragged. Processing all MKV files in the current folder.
     for %%F in (*.mkv) do (
         echo Processing: %%F
-        ffmpeg -i "%%F" -filter_complex "[0:v]setpts=!video_pts!*PTS[v];[0:a]atempo=!speedup_ratio![a]" -map "[v]" -map "[a]" "%%~nF_speedup.mkv"
+        ffmpeg -i "%%F" -filter_complex %filter_complex_expr% -map "[v]" -map "[a]" "%%~nF_%postfix%.mkv"
     )
 ) else (
     echo Files dragged. Processing pulled files.
     :process_args
     if "%~1"=="" goto :done
     echo Processing: %~1
-    ffmpeg -i "%~1" -filter_complex "[0:v]setpts=!video_pts!*PTS[v];[0:a]atempo=!speedup_ratio![a]" -map "[v]" -map "[a]" "%~n1_speedup.mkv"
+    ffmpeg -i "%~1" -filter_complex %filter_complex_expr% -map "[v]" -map "[a]" "%~n1_%postfix%.mkv"
     shift
     goto :process_args
 )
