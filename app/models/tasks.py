bd1ce76e2de6371fb1876f060b18f569e70a4605
superyngo <superyngo@gmailc.om>
2024-12-02 15:46:13 +0800

    update model

--- app/models/tasks.py
+++ app/models/tasks.py
@@ -1,14 +1,8 @@
 from typing import TypedDict
 from pathlib import Path
-
-class CovertMideo(TypedDict):
-    name:str
-    foler:Path
-
 class CsBasicComponent:
     def __getattr__(self, name):
         raise AttributeError(f"'{self.__class__.__name__}' '{name}' was not set")
-
 
 class MyDataclass:
     def __init__(self, **kwargs) -> None:
@@ -21,6 +15,24 @@ class MyDataclass:
         if name in self.__slots__:
             raise AttributeError(f"'{name}' was not set during initialization")
         raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+class MideoMerger(TypedDict):
+    name: str
+    folder: Path
+    start_hour: int
+    delete_after: bool
+
+class speedup(TypedDict):
+    name: str
+    folder: Path
+    multiple: int
+
+class GPhotoUploader(TypedDict):
+    name: str
+    profile: Path
+    local_album: Path
+    GPhoto_url: str
+
 
 
 
